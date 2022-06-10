57aa643a50c3f4a8f575e6a5c4162dccde6b472b
BAIRITHARUN <byri.tharun@gmail.com>
2022-06-10 22:15:38 +0530

    Update mocation.tsx

--- util/mocation.tsx
+++ util/mocation.tsx
@@ -1,12 +1,10 @@
-const GoOGLE_API_KEY = 'AIzaSyCjgu_A6akLj3IVgpw4mnHT5cRzeLPN2YM';
 
 
-//https://maps.googleapis.com/maps/api/staticmap?center=17.9812564,79.6168025&zoom=13&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7c40.702147,-74.015794&markers=color:green%7Clabel:G7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyCjgu_A6akLj3IVgpw4mnHT5cRzeLPN2YM
-
+//https://maps.googleapis.com/maps/api/staticmap?center=17.9812564,79.6168025&zoom=13&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7c40.702147,-74.015794&markers=color:green%7Clabel:G7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=Google_API_Key
 
 
 export function getMapPreview () {
-    // const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=17.9812564,79.6168025&zoom=13&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7c17.9812564,79.6168025&key=AIzaSyCjgu_A6akLj3IVgpw4mnHT5cRzeLPN2YM`
+    // const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=17.9812564,79.6168025&zoom=13&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7c17.9812564,79.6168025&key=Google_API_Key`
     // return mapUrl
 
 }
